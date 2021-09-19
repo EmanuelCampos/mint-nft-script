9f469707ffcfd3e0c052388d579e87edf75fcd31
EmanuelCampos <contatoferreirads@gmail.com>
2021-09-19 06:54:08 -0300

    boilerplate

--- src/mintNFT.ts
+++ src/mintNFT.ts
@@ -1,42 +1,6 @@
 import { ethers } from "ethers";
-import { getProvider } from "./getProvider";
-
-import FactoryNFT from "../artifacts/contracts/factoryNFT.sol/FactoryNFT.json";
-import { FactoryNFTAddress } from "./contractAddresses";
 
 const run = async () => {
-  const [, , ...unsanitizedArgs] = process.argv;
-
-  if (unsanitizedArgs.length !== 2) {
-    // eslint-disable-next-line
-    console.log("yarn es ./src/mintNft <privatekey> <tokenuri>");
-    return;
-  }
-
-  const [privateKey, tokenUri] = unsanitizedArgs;
-
-  const provider = getProvider();
-
-  const wallet = new ethers.Wallet(privateKey, provider);
-
-  const factoryNFTContract = new ethers.Contract(
-    FactoryNFTAddress,
-    FactoryNFT.abi,
-    wallet
-  );
-
-  console.log("minting");
-  const transaction = await factoryNFTContract.createToken(tokenUri);
-  const tx = await transaction.wait();
-  const event = tx.events[0];
-  const value = event.args[2];
-  const tokenId = value.toNumber();
-
-  console.log({
-    transaction,
-    tx,
-    tokenId,
-  });
 };
 
 (async () => {
